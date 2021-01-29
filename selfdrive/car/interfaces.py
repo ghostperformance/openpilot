10839ec7faac76d04e2feeb38818ed09b54e1edf
ghostperformance <70942642+ghostperformance@users.noreply.github.com>
2021-01-29 17:44:29 -0500

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -97,12 +97,12 @@ class CarInterfaceBase():
 
     if cs_out.doorOpen:
       events.add(EventName.doorOpen)
-    if cs_out.seatbeltUnlatched:
-      events.add(EventName.seatbeltNotLatched)
+    #if cs_out.seatbeltUnlatched:
+     # events.add(EventName.seatbeltNotLatched)
     if self.dragonconf.dpGearCheck and cs_out.gearShifter != GearShifter.drive and cs_out.gearShifter not in extra_gears:
       events.add(EventName.wrongGear)
-    if cs_out.gearShifter == GearShifter.reverse:
-      events.add(EventName.reverseGear)
+    #if cs_out.gearShifter == GearShifter.reverse:
+     # events.add(EventName.reverseGear)
     if not self.dragonconf.dpAtl and not cs_out.cruiseState.available:
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
